abbb16831260195ef90ba8dd8cb299b182f206e7
0xTovand <tovand@protonmail.com>
2022-09-16 00:16:39 +0300

    -react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,12 @@
-import {
-  Hydrate,
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { AppProps } from 'next/app';
-import { useState } from 'react';
 import Layout from '../components/Layout';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [queryClient] = useState(() => new QueryClient());
-
   return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </Hydrate>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 
